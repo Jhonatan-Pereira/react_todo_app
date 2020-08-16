db57c19bfad50ff196180b1d6abb8930a358fab0
Jhonatan Frade <jhonatanfrade.pereira@gmail.com>
2020-08-16 20:52:35 -0300

    Components Todo e About

---
+++ src/todo/todo.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Todo extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Teste2</h1>
+            </div>
+        )
+    }
+}
